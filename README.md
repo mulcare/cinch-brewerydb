83d2b9fad1d38ca213f598f7cf2135b27d270e35
ross mulcare <ross.mulcare@gmail.com>
2016-05-23 14:34:56 -0400

    brief documentation added

---
+++ README.md
@@ -0,0 +1,17 @@
+# cinch-brewerydb - a brewerydb.com plugin for Cinch
+
+## Description
+
+This is a small plugin for the Cinch IRC bot framework intended to query the
+brewerydb.com API and return some information about a beer.
+
+### Example Usage
+
+```
+<user> !beer la fin du monde
+<bot> La Fin Du Monde by Unibroue (Chambly, Québec)
+<bot> Belgian Tripel - 9% ABV - 19 IBU 🍺
+```
+
+## Dependencies
+* [Cinch](https://github.com/cinchrb/cinch) (`gem install cinch`)
--- plugins/brewerydb.rb
+++ plugins/brewerydb.rb
@@ -1,3 +1,48 @@
+# = Cinch BreweryDB API query plugin
+# Queries the BreweryDB beer database (http://www.brewerydb.com/) with the
+# name of a beer and returns some information about that beer.
+#
+# == Configuration
+# Add the following to your bot's configure.do stanza:
+#
+#   config.plugins.options[Cinch::BreweryDB] = {
+#     :brewerydb_api_key => ""
+#   }
+#
+# [brewerydb_api_key]
+#   API keys are available after creating an account on brewerydb. See:
+#   http://www.brewerydb.com/developers/apps
+#
+# == Author
+# Ross Mulcare (@mulcare)
+#
+# == Notes
+# Thanks to Marvin Gülker (Quintus) for his numerous Cinch plugins that I have
+# learned and borrowed from. The above comments/documentation are cribbed from
+# his style. See: https://github.com/Quintus/cinch-plugins
+#
+# == License
+# The MIT License (MIT)
+# Copyright (c) 2016 Ross Mulcare
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 require 'net/http'
 require 'json'
 
